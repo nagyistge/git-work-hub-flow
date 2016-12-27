44b9214755bcfbf7b59ca2cac66a7f1022a7752f
Patrick <patrick@pdsullivan.com>
2016-12-26 16:54:09 -0800

    initial commit

---
+++ app/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- <img class="logo" src="./assets/logo.png"> -->
+    <hello></hello>
+    <!-- <stuff></stuff> -->
+  </div>
+</template>
+
+<script>
+  import Hello from './components/Hello'
+  import Stuff from './components/Stuff'
+  export default {
+    components: {
+      Hello,
+      Stuff
+    }
+  }
+</script>
+
+<style>
+  html {
+    height: 100%;
+  }
+  body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  #app {
+    margin-top: -100px;
+    max-width: 600px;
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+  }
+  .logo {
+    width: 100px;
+    height: 100px
+  }
+</style>
